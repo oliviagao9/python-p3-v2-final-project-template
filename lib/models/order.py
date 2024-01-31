0d43ad032677a55622cba706589e5961d4c4b3a9
Oliva Gao <oliviagao9@gmail.com>
2024-01-30 18:47:03 -0600

    updated order class instance and class methods

--- lib/models/order.py
+++ lib/models/order.py
@@ -86,9 +86,69 @@ class Order:
         breakpoint()
         self.id = CURSOR.lastrowid
         type(self).all[self.id] = self
-    
+
+    @classmethod
+    def update_quantity(cls, order_id, quantity):
+        old_order_quantity = Order.find_by_id(order_id).quantity
+        sql = """
+            UPDATE orders
+            SET quantity = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (quantity, order_id))
+        CONN.commit()
+
+        product_id = Order.find_by_id(order_id).product_id
+
+        original_quantity = Inventory.find_by_id(product_id).quantity
+        new_inventory_quantity = original_quantity - old_order_quantity + quantity
+        Inventory.update_quantity(product_id, new_inventory_quantity)
+
+       
     @classmethod
     def create(cls, name, quantity, product_id):
         order = cls(name, quantity, product_id)
         order.save()
+
+        original_quantity = Inventory.find_by_id(product_id).quantity
+        Inventory.update_quantity(product_id, quantity + original_quantity)
         return order
+    
+    @classmethod
+    def instance_from_db(cls, row):
+
+        # Check the dictionary for  existing instance using the row's primary key
+        order = cls.all.get(row[0])
+        if order:
+            # ensure attributes match row values in case local instance was modified
+            order.name = row[1]
+            order.quantity = row[2]
+            order.product_id = row[3]
+        else:
+            # not in dictionary, create new instance and add to dictionary
+            order = cls(row[1], row[2], row[3])
+            order.id = row[0]
+            cls.all[order.id] = order
+        return order
+    
+    @classmethod
+    def get_all(cls):
+        sql = """
+            SELECT *
+            FROM orders
+        """
+
+        rows = CURSOR.execute(sql).fetchall()
+
+        return [cls.instance_from_db(row) for row in rows]
+
+    @classmethod
+    def find_by_id(cls, id):
+        sql = """
+            SELECT *
+            FROM orders
+            WHERE id = ?
+        """
+
+        row = CURSOR.execute(sql, (id,)).fetchone()
+        return cls.instance_from_db(row) if row else None
